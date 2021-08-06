7afc07edee1df545e331c09216907bbbeab8af5a
tkminek <tomas.kminek@seznam.cz>
2021-08-06 12:29:39 +0200

    first pattern

--- pDF_reader_new.py
+++ pDF_reader_new.py
@@ -3,6 +3,8 @@ import cv2
 import pytesseract
 import numpy as np
 from pytesseract import Output
+import re
+from re import finditer
 
 def has_numbers(inputString):
     return any(char.isdigit() for char in inputString)
@@ -33,10 +35,7 @@ def uprava_jpg(jpg):
     image = cv2.erode(image, kernel, iterations=1)
     return(image)
 
-def vypis_txt(text,name):
-    file_text=open("PDF/"+str(name)+".txt","a+",encoding='utf-8')
-    file_text.write(text)
-    file_text.close()
+
 
 def prijata_faktura_txt(name):
     number_pages=pdf_page_number(name)
@@ -44,45 +43,24 @@ def prijata_faktura_txt(name):
     for index in range(0,number_pages):
         jpg=create_jpg(name,index)
         jpg_upravene=uprava_jpg(jpg)
-        my_config=r"--psm 4"
-        #my_config='--psm 6'
-        text=pytesseract.image_to_string(jpg_upravene,lang='ces',config=my_config)
+        my_config=r'--oem 3 --psm 6 '
+        text = pytesseract.image_to_string(jpg_upravene,lang='ces',config=my_config)
         #only for spyder start#
         arr = text.split('\n')[0:-1]        
         text = '\n'.join(arr)
         #only for spyder end#
-        vypis_txt(text,name)
-        print("---------------")
-        print(f"TXT file - Page number: {index} is DONE")    
+        print(text)
+        #print("---------------")
+        #print(f"TXT file - Page number: {index} is DONE")    
 
-def info_txt(name):    
-    file_text=open("PDF/"+str(name)+".txt","r",encoding='utf-8')
-    dodavatel_list=[]
-    odberatel_list=[]
-    odberatel_ident=["odberatel","odběratel"]
-    dodavatel_ident=["dodavatel"]
-    for index,line in enumerate(file_text):        
-        if any(x in line.lower() for x in odberatel_ident):
-            odberatel_list.append(index)
-        elif any(x in line.lower() for x in dodavatel_ident):
-            dodavatel_list.append(index)   
-            
-    return(dodavatel_list,odberatel_list)        
 
-def faktura_zpracovani(name):
-    dodavatel,odberatel=info_txt(name)
-    print(f"dodavatel : {dodavatel}")
-    print(f"odberatel : {odberatel}")
    
 
 
 def main():
-    name="faktury_prijate"
+    name="faktura1"
     prijata_faktura_txt(name)
-    #faktura_zpracovani(name)
 
-    #cv2.imshow('image',jpg_upravene)
-    #cv2.waitKey(0)
 
 
 if __name__ == '__main__':
